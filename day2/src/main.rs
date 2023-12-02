06f7880e542c489c2ca9c7b998e308778265202e
George Nicoll <george.nicoll@gmail.com>
2023-12-02 08:46:32 +0000

    Day2 Before Any Tidy up

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,23 +1,31 @@
-use processor::process;
+use std::collections::{HashMap, HashSet};
+
+use once_cell::sync::Lazy;
+use processor::{process, read_word, read_int, ok_identity};
 
 type AError = anyhow::Error;
-type InitialState = Vec<String>;
+type InitialState = Vec<Game>;
 type LoadedState = InitialState;
-type ProcessedState = LoadedState;
-type FinalResult = ProcessedState;
+type ProcessedState = i64;
+
+#[derive(Debug)]
+struct Game {
+    number: i64,
+    picks: Vec<HashMap<String, i64>>,
+}
 
 fn main() {
-    let file = "test-input.txt";
+    //let file = "test-input.txt";
     //let file = "test-input2.txt";
-    //let file = "input.txt";
+    let file = "input.txt";
 
     let result1 = process(
         file,
         Vec::new(),
         parse_line,
         finalise_state,
-        perform_processing,
-        calc_result,
+        perform_processing_1,
+        ok_identity,
     );
     match result1 {
         Ok(res) => println!("Result 1: {:?}", res),
@@ -29,8 +37,8 @@ fn main() {
         Vec::new(),
         parse_line,
         finalise_state,
-        perform_processing,
-        calc_result,
+        perform_processing_2,
+        ok_identity,
     );
     match result2 {
         Ok(res) => println!("Result 2: {:?}", res),
@@ -38,19 +46,90 @@ fn main() {
     }
 }
 
+static DELIMITERS: Lazy<HashSet<char>> = Lazy::new(||
+    HashSet::from([' ', ':', ',', ';'])
+);
+
 fn parse_line(mut state: InitialState, line: String) -> Result<InitialState, AError> {
-    state.push(line);
+    let mut chars = line.chars();
+    //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    if let Some(_game) = read_word(&mut chars, &DELIMITERS) {
+        let (number, _delimiter) = read_int(&mut chars, &DELIMITERS)?;
+        let mut picks = Vec::new();
+        let mut cubes: HashMap<String, i64> = HashMap::new();
+
+        let mut num_cubes_and_delimiter = read_int(&mut chars, &DELIMITERS);
+        while num_cubes_and_delimiter.is_ok() {
+            let (num_cubes, _) = num_cubes_and_delimiter.as_ref().ok().unwrap();
+            let (colour, delimiter) = read_word(&mut chars, &DELIMITERS)
+                .ok_or_else(|| AError::msg(format!("Expected a colour after {} in '{}'", num_cubes, line)))?;
+            cubes.insert(colour, num_cubes.clone());
+            let end_of_pick = delimiter.map(|c| c == ';').unwrap_or(true);
+            if end_of_pick {
+                picks.push(cubes);
+                cubes = HashMap::new();
+            }
+            num_cubes_and_delimiter = read_int(&mut chars, &DELIMITERS);
+        }
+
+        state.push(Game { number, picks });
+    }
     Ok(state)
 }
 
 fn finalise_state(state: InitialState) -> Result<LoadedState, AError> {
+    // state.iter().for_each(|game| {
+    //     println!("{:?}", game);
+    // });
     Ok(state)
 }
 
-fn perform_processing(state: LoadedState) -> Result<ProcessedState, AError> {
-    Ok(state)
+fn perform_processing_1(state: LoadedState) -> Result<ProcessedState, AError> {
+    let max_cubes: HashMap<String, i64> = HashMap::from([
+        ("red".into(), 12.into()),
+        ("green".into(), 13.into()),
+        ("blue".into(), 14.into())
+    ]);
+
+    let possible_games = state.iter()
+        .filter(|game| {
+            game.picks.iter().all(|pick| {
+                max_cubes.iter().all(|(max_colour, max_number)| {
+                    pick.get(max_colour).map(|number| number <= max_number).unwrap_or(true)
+                })
+            })
+        })
+        .map(|game| {
+            //println!("Possible: {:?}", game);
+            game.number
+        })
+        .fold(0, |acc, number| acc + number);
+
+    Ok(possible_games)
 }
 
-fn calc_result(state: ProcessedState) -> Result<FinalResult, AError> {
-    Ok(state)
+fn perform_processing_2(state: LoadedState) -> Result<ProcessedState, AError> {
+    let powers = state.iter()
+        .map(|game| {
+            let mut max_cubes: HashMap<String, i64> = HashMap::from([
+                ("red".into(), 0.into()),
+                ("green".into(), 0.into()),
+                ("blue".into(), 0.into())
+            ]);
+            game.picks.iter().for_each(|pick| {
+                pick.iter().for_each(|(colour, number)| {
+                    let current_max = max_cubes.get_mut(colour).unwrap();
+                    if *current_max < *number {
+                        *current_max = *number;
+                    }
+                })
+            });
+            let power = max_cubes.iter().fold(1, |acc, (_colour, number)| {
+                acc * number
+            });
+            power
+        })
+        .collect::<Vec<i64>>();
+    let result = powers.iter().fold(0, |acc, power| acc + power);
+    Ok(result)
 }
--- processor/src/lib.rs
+++ processor/src/lib.rs
@@ -1,16 +1,21 @@
 use std::{
     fs::File,
-    io::{BufRead, BufReader},
+    io::{BufRead, BufReader}, str::Chars, collections::HashSet,
 };
+
+use anyhow::Context;
+
+type AError = anyhow::Error;
+type Delimiter = char;
 
 pub fn process<LoadState, State, ProcessedState, FinalResult>(
     file_name: &str,
     initial_state: LoadState,
-    parse_line: fn(LoadState, String) -> Result<LoadState, anyhow::Error>,
-    finalise_state: fn(LoadState) -> Result<State, anyhow::Error>,
-    perform_processing: fn(State) -> Result<ProcessedState, anyhow::Error>,
-    calc_result: fn(ProcessedState) -> Result<FinalResult, anyhow::Error>,
-) -> Result<FinalResult, anyhow::Error> {
+    parse_line: fn(LoadState, String) -> Result<LoadState, AError>,
+    finalise_state: fn(LoadState) -> Result<State, AError>,
+    perform_processing: fn(State) -> Result<ProcessedState, AError>,
+    calc_result: fn(ProcessedState) -> Result<FinalResult, AError>,
+) -> Result<FinalResult, AError> {
     let file = File::open(file_name)?;
     let loaded_state = BufReader::new(file)
         .lines()
@@ -21,13 +26,51 @@ pub fn process<LoadState, State, ProcessedState, FinalResult>(
     calc_result(processed_state)
 }
 
-pub fn ok_identity<T>(t: T) -> Result<T, anyhow::Error> {
+pub fn ok_identity<T>(t: T) -> Result<T, AError> {
     Ok(t)
 }
 
 pub fn reverse(s: &str) -> String {
     //assume no graphemes - use unicode_segmentation if this is not the case
     s.chars().rev().collect()
+}
+
+// Read a word for the current positions of chars, advancing to the next non-delimiter and reading to the end
+// or the next delimiter
+pub fn read_word(chars: &mut Chars<'_>, delimiters: &HashSet<Delimiter>) -> Option<(String, Option<Delimiter>)> {
+    let mut consumed: Vec<char> = Vec::new();
+    let mut next: Option<char> = Some('/');
+    while next.is_some() {
+        next = chars.next();
+        if next.is_none() {
+            break;
+        }
+        let c = next.unwrap();
+        if delimiters.contains(&c) {
+            if consumed.is_empty() {
+                continue;
+            } else {
+                break;
+            }
+        }
+        consumed.push(c);
+    }
+    if consumed.is_empty() {
+        None
+    } else {
+        Some((consumed.iter().collect(), next))
+    }
+}
+
+pub fn read_int(chars: &mut Chars<'_>, delimiters: &HashSet<Delimiter>) -> Result<(i64, Option<Delimiter>), AError> {
+    read_word(chars, delimiters)
+        .ok_or_else(|| AError::msg("No word found to convert to integer"))
+        .and_then(|word_and_delimiter| {
+            let (word, delimiter) = word_and_delimiter;
+            word.parse::<i64>()
+                .map(|i| (i, delimiter))
+                .context(format!("Failed to parse '{}' to integer", word))
+        })
 }
 
 #[cfg(test)]
